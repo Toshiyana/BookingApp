a88bdbd9e75cd010438acf4a131d68a1c0e155e8
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-23 04:59:51 +0900

    Add driver package

---
+++ driver/driver.go
@@ -0,0 +1,67 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	// By defining DB type, you can change kinds of databases, postgres, mariaDB, etc.
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10 // max connections to the database open at any given time.
+const maxIdleDbConn = 5  // the number of connections remained in the pool
+const maxDbLifetime = 5 * time.Minute
+
+// ConnectSQL creates database pool for Postgres
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		// In error, I don't want anything else to start up, so use panic()
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+
+	err = testDB(d)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
+
+// testDB tries to ping the database
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase creates a new database for the application
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
