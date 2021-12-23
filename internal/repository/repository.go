bcbb737dd8afdcc43966c042b5b30b4dc932943a
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-24 05:54:39 +0900

    Add searching for availability by room

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/Toshiyana/BookingApp/internal/models"
+import (
+	"time"
+
+	"github.com/Toshiyana/BookingApp/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(roomID int, start, end time.Time) (bool, error)
 }
