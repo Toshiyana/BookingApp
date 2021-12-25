af46a33ee9bf72b24806867cfa9f15de75932d56
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-26 04:24:15 +0900

    Format email using Foundation & Update test

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -52,6 +52,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -68,6 +74,14 @@ func TestMain(m *testing.M) {
 	//----------------------------------------------------------------s
 
 	os.Exit(m.Run())
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
