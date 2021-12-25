b3c78d74f4b386d92fb22e9093c62337d16a4b31
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-26 04:24:39 +0900

    Format email using Foundation & Update test

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -41,15 +41,6 @@ func main() {
 	fmt.Println("Starting mail listener...")
 	listenForMail()
 
-	msg := models.MailData{
-		To:      "john@do.ca",
-		From:    "me@here.com",
-		Subject: "Some subject",
-		Content: "Hi, everyone",
-	}
-
-	app.MailChan <- msg
-
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	// Use routes
