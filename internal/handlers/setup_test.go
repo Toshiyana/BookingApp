acf405f183b23716f85f65ed978994a5ffc5e242
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-01-03 02:30:40 +0900

    Update tests in handlers

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -20,7 +20,12 @@ import (
 	"github.com/justinas/nosurf"
 )
 
-var functions = template.FuncMap{}
+var functions = template.FuncMap{
+	"humanDate":  render.HumanDate,
+	"formatDate": render.FormatDate,
+	"iterate":    render.Iterate,
+	"add":        render.Add,
+}
 
 var app config.AppConfig
 var session *scs.SessionManager
@@ -32,6 +37,10 @@ func TestMain(m *testing.M) {
 	//----------------------------------------------------------------
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
+	gob.Register(models.User{})
+	gob.Register(models.Room{})
+	gob.Register(models.Restriction{})
+	gob.Register(map[string]int{})
 
 	// change this to true when in production
 	app.InProduction = false
@@ -79,50 +88,12 @@ func TestMain(m *testing.M) {
 func listenForMail() {
 	go func() {
 		for {
-			_ = <-app.MailChan
+			<-app.MailChan
 		}
 	}()
 }
 
 func getRoutes() http.Handler {
-	//----------------------------------------------------------------
-	// run() in main.go
-	//----------------------------------------------------------------
-	// what am I going to put in the session
-	gob.Register(models.Reservation{})
-
-	// change this to true when in production
-	app.InProduction = false
-
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	app.InfoLog = infoLog
-
-	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	app.ErrorLog = errorLog
-
-	// use session in handlers
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour // last for 24 hours
-	// set cookie parameters because all session use cookies in one form.
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction
-
-	app.Session = session
-
-	tc, err := CreateTestTemplateCache()
-	if err != nil {
-		log.Fatal("cannot create template cache")
-	}
-
-	app.TemplateCache = tc
-	// In develop mode, Usecache sets false because of reloding templates. <- check templates changed.
-	// In release mode, Usecashe sets true because of not reloding templates.
-	app.UseCache = true // In test, not to call CreateTemplateCache() and use pathToTemplates in render.go, set true
-
-	repo := NewTestRepo(&app)
-	NewHandlers(repo)
-	render.NewRenderer(&app)
 	//----------------------------------------------------------------
 	// routes() in routes.go
 	//----------------------------------------------------------------
@@ -135,20 +106,36 @@ func getRoutes() http.Handler {
 	// mux.Use(NoSurf) // When testing handlers_test.go, you don't have to use NoSurf because of CSRF
 	mux.Use(SessionLoad)
 
-	mux.Get("/", http.HandlerFunc(Repo.Home))
-	mux.Get("/about", http.HandlerFunc(Repo.About))
-	mux.Get("/generals-quarters", http.HandlerFunc(Repo.Generals))
-	mux.Get("/majors-suite", http.HandlerFunc(Repo.Majors))
+	mux.Get("/", Repo.Home)
+	mux.Get("/about", Repo.About)
+	mux.Get("/generals-quarters", Repo.Generals)
+	mux.Get("/majors-suite", Repo.Majors)
 
-	mux.Get("/make-reservation", http.HandlerFunc(Repo.Reservation))
-	mux.Post("/make-reservation", http.HandlerFunc(Repo.PostReservation))
-	mux.Get("/reservation-summary", http.HandlerFunc(Repo.ReservationSummary))
+	mux.Get("/make-reservation", Repo.Reservation)
+	mux.Post("/make-reservation", Repo.PostReservation)
+	mux.Get("/reservation-summary", Repo.ReservationSummary)
 
-	mux.Get("/search-availability", http.HandlerFunc(Repo.Availability))
-	mux.Post("/search-availability", http.HandlerFunc(Repo.PostAvailability))
-	mux.Post("/search-availability-json", http.HandlerFunc(Repo.AvailabilityJSON))
+	mux.Get("/search-availability", Repo.Availability)
+	mux.Post("/search-availability", Repo.PostAvailability)
+	mux.Post("/search-availability-json", Repo.AvailabilityJSON)
 
-	mux.Get("/contact", http.HandlerFunc(Repo.Contact))
+	mux.Get("/contact", Repo.Contact)
+
+	mux.Get("/user/login", Repo.ShowLogin)
+	mux.Post("/user/login", Repo.PostShowLogin)
+	mux.Get("/user/logout", Repo.Logout)
+
+	mux.Get("/admin/dashboard", Repo.AdminDashboard)
+
+	mux.Get("/admin/reservations-new", Repo.AdminNewReservations)
+	mux.Get("/admin/reservations-all", Repo.AdminAllReservations)
+	mux.Get("/admin/reservations-calendar", Repo.AdminReservationsCalendar)
+	mux.Post("/admin/reservations-calendar", Repo.AdminPostReservationsCalendar)
+	mux.Get("/admin/process-reservation/{src}/{id}/do", Repo.AdminProcessReservation)
+	mux.Get("/admin/delete-reservation/{src}/{id}/do", Repo.AdminDeleteReservation)
+
+	mux.Get("/admin/reservations/{src}/{id}/show", Repo.AdminShowReservation)
+	mux.Post("/admin/reservations/{src}/{id}", Repo.AdminPostShowReservation)
 
 	// create a file server, a place to get static files
 	fileServer := http.FileServer(http.Dir("./static/"))
@@ -188,26 +175,29 @@ func CreateTestTemplateCache() (map[string]*template.Template, error) {
 
 	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
 	if err != nil {
+		log.Println(err)
 		return myCache, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		fmt.Println("Page is currently", page)
 
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
+			log.Println(err)
 			return myCache, err
 		}
 
 		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 		if err != nil {
+			log.Println(err)
 			return myCache, err
 		}
 
 		if len(matches) > 0 {
 			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 			if err != nil {
+				log.Println(err)
 				return myCache, err
 			}
 		}
